2b61f20ba217fe5e2b9a0c6c070d76c8606995b7
Rene Funcion <rcfun959@gmail.com>
2023-03-23 05:55:11 -0700

    Completed phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(arrayDriversNames, strName) {
+    return arrayDriversNames.filter(name => name.toLowerCase() === strName.toLowerCase());
+}
+function fuzzyMatch(arrayDriversNames, strName) {
+    return arrayDriversNames.filter(name => name.indexOf(strName, 0) === 0);
+}
+function matchName(arrayDriverObj, strName) {
+    return arrayDriverObj.filter(driver => driver.name === strName);
+}
